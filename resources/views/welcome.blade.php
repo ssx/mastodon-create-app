ee3d8924d010e9ec5d060a99621672c4ca0a2680
Scott Wilcox <scott@dor.ky>
2017-04-19 18:56:10 +0100

    Initial commit of application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,103 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Mastodon Create OAuth2 Application</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #000000;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
+                line-height: 25px;
+            }
+            .container {
+                width: 600px;
             }
 
-            .full-height {
-                height: 100vh;
+            .form-check-inline { float: left; margin-right: 20px; }
+
+            footer {
+                border-top: 1px solid #ccc;
+                margin-top: 40px;
+                padding-top: 10px;
             }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .github-img {
+                height: 30px;
+                display: inline;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+        <div class="container">
+            <form>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="col-md-12 text-center">
+                    <br><img width="200" class="align-center" src="https://assets.mastodon.social/assets/fluffy-elephant-friend-6b47d8e924332955795ff4b2d8fc446437d26b28bfc67d6be2a4d88995ab2c1f.png" alt="Mastodon">
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-md-12">
+
+                    <h2>Create Mastodon OAuth2 Application</h2>
+                    <p>You can use the form below to easily create an OAuth2 application on any Mastodon instance. This
+                        will return you a <code>client_id</code> and <code>client_secret</code> for you to then use in
+                        your application.</p>
+                    <br>
+                    <div class="form-group">
+                        <label for="iptInstance">Mastodon Instance</label>
+                        <input type="text" id="iptInstance" class="form-control" placeholder="mastodon.social" required autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="iptInstance">Application Name</label>
+                        <input type="text" id="iptInstance" class="form-control" placeholder="My Awesome Mastodon App" required autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="iptInstance">Redirect URI</label>
+                        <input type="text" id="iptInstance" class="form-control" placeholder="https://your.site/callback" required autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="">
+                            <label>Scopes</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="scopes[]" value="read"> Read
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="scopes[]" value="write"> Write
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="scopes[]" value="follow" disabled> Follow
+                            </label>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
+                <div class="col-md-12">
+                    <br>
+                    <button class="btn btn-md btn-primary" type="submit">Create Application</button>
+                    <button class="btn btn-md btn-info" type="submit">Show Curl Command</button>
+                </div>
+
+                <div class="col-md-12">
+                    <footer class="text-center">
+                        Built by <a href="https://dor.ky">Scott Wilcox</a> &middot; Feedback via <a href="https://github.com/ssx/mastodon-create-app"><img class="github-img" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" alt="Github"></a>
+                    </footer>
+                </div>
+            </form>
         </div>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
